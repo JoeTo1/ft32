3a23026240b120a27d9dd8a4883ac93e059dc40c
Joe <joe.pal@gmx.net>
2018-03-01 23:14:24 +0100

    Add Network Handler

---
+++ ft32/sloeber.ino.cpp
@@ -0,0 +1,23 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-03-01 23:09:12
+
+#include "Arduino.h"
+#include "AssetHandler.h"
+#include "WebsocketHandler.h"
+#include "NetworkHandler.h"
+#include "ft_ESP32_SHM.h"
+#include "ft_ESP32_SW_Queue.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+void initQueue_static(void* arg) ;
+void setup() ;
+void loop() ;
+
+#include "ft32.ino"
+
+
+#endif
